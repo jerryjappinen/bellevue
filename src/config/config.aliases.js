82bfb4f882cabce38f1101b0debed2bd5a690dd7
Jerry Jäppinen <eiskis@gmail.com>
2017-12-14 20:17:12 +0100

    Added aliases

---
+++ src/config/config.aliases.js
@@ -0,0 +1,40 @@
+
+// Aliases usable in codebase when doing imports and resolving URLs. From project root.
+// NOTE: unlike other configuration files, this is needed by the low-level tooling scripts BEFORE any aliases have been defined and full URL resolution is working (since those aliases are defined here, duh). That's why this is split from other configuration files.
+module.exports = {
+
+	// src root
+	// NOTE: prefer the other aliases over
+	'@': 'src',
+
+	// The base configuration (alias is mostly for the client)
+	// NOTE: for client-side code it would be better to use a JS utility that reads configuration (merging values from multiple sources) instead of using these raw values in application code
+	'@config': 'src/config',
+	// '@locales': 'src/locales',
+
+	// Vendor code, services, utilities etc.
+	// '@models': 'src/models',
+	// '@services': 'src/services',
+	'@util': 'src/util',
+	// '@vendor': 'src/vendor',
+
+	// Assets
+	'@assets': 'src/assets',
+	// '@fonts': 'src/fonts',
+	'@svg': 'src/svg',
+
+	// Vue application code
+	'@components': 'src/components',
+	'@directives': 'src/vue-directives',
+	'@mixins': 'src/vue-mixins',
+	'@plugins': 'src/vue-plugins',
+
+	// Global styles
+	// NOTE: we could split this further
+	'@global-styles': 'src/styles/global.scss',
+	'@shared-styles': 'src/styles/shared.scss'
+
+	// Test cases
+	// '@test': 'test'
+
+}
--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
+// import aliases from './config.aliases'
 import manifest from './config.manifest'
 import meta from './config.meta'
 import routes from './config.routes'
 import svgo from './config.svgo'
 
 export {
+	// aliases,
 	manifest,
 	meta,
 	routes,
