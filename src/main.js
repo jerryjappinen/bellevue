6621cf99a82e90e1864f49d15a4578c5e1f172c2
Jerry Jäppinen <eiskis@gmail.com>
2017-12-14 13:39:15 +0100

    Global styles

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// Global styles
+import './styles/global.scss'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
