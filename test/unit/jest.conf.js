51ed3d44f07d685f67cb8db074379c9e714acdea
Jerry Jäppinen <eiskis@gmail.com>
2017-12-14 21:13:12 +0100

    Jest aliases

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -1,4 +1,37 @@
+const _ = require('lodash')
 const path = require('path')
+
+// We need to turn aliases to something like this for Jest:
+// moduleNameMapper: {
+//   '^@/(.*)$': '<rootDir>/src/$1'
+// },
+
+const aliasConfig = require('../../src/config/config.aliases.js')
+
+function escapeRegexp (str) {
+  return str
+}
+
+function treatAliasKey (aliasValue, aliasKey) {
+  return '^' + escapeRegexp(aliasKey) + '/(.*)$'
+}
+
+function treatAliasValue (aliasValue) {
+  return '<rootDir>/' + escapeRegexp(aliasValue) + '/$1'
+}
+
+function treatAliasKeySingle (aliasValue, aliasKey) {
+  return '^' + escapeRegexp(aliasKey)
+}
+
+function treatAliasValueSingle (aliasValue) {
+  return '<rootDir>/' + escapeRegexp(aliasValue)
+}
+
+let jestAliases = _.mapKeys(_.mapValues(aliasConfig, treatAliasValue), treatAliasKey)
+_.merge(jestAliases, _.mapKeys(_.mapValues(aliasConfig, treatAliasValueSingle), treatAliasKeySingle))
+
+console.log('jestAliases', jestAliases)
 
 module.exports = {
   rootDir: path.resolve(__dirname, '../../'),
@@ -7,11 +40,10 @@ module.exports = {
     'json',
     'vue'
   ],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  },
+  moduleNameMapper: jestAliases,
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(scss)$': '<rootDir>/node_modules/jest-css-modules',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
   },
   testPathIgnorePatterns: [
@@ -23,8 +55,8 @@ module.exports = {
   coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
+    '!src/svg/index.js',
     '!src/main.js',
-    '!src/router/index.js',
     '!**/node_modules/**'
   ]
 }
