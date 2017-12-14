ff7dab4e9d4ef9809464bb9ea5076b888a81e268
Jerry Jäppinen <eiskis@gmail.com>
2017-12-14 16:46:43 +0100

    Added transition helper components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
 import App from './App'
 import HelloWorld from './HelloWorld'
 
+import CustomTransition from './transitions/CustomTransition'
+import Fade from './transitions/Fade'
+
 export {
 	App,
-	HelloWorld
+	HelloWorld,
+
+	CustomTransition,
+	Fade
 }
---
+++ src/components/transitions/Fade.vue
@@ -0,0 +1,18 @@
+<script>
+	export default {
+		name: 'fade',
+
+		props: [
+			'name',
+			'mode',
+			'appear'
+		]
+
+	}
+</script>
+
+<template>
+	<custom-transition name="fade" :mode="mode" :appear="appear"><slot></slot></custom-transition>
+</template>
+
+<style lang="scss"></style>
