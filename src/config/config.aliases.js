b71484a68aa5fa3af96e306bb3f28862f8dd13e9
Jerry Jäppinen <eiskis@gmail.com>
2017-12-14 21:10:18 +0100

    Moved vue setup from main.js

--- src/config/config.aliases.js
+++ src/config/config.aliases.js
@@ -3,10 +3,6 @@
 // NOTE: unlike other configuration files, this is needed by the low-level tooling scripts BEFORE any aliases have been defined and full URL resolution is working (since those aliases are defined here, duh). That's why this is split from other configuration files.
 module.exports = {
 
-	// src root
-	// NOTE: prefer the other aliases over
-	'@': 'src',
-
 	// The base configuration (alias is mostly for the client)
 	// NOTE: for client-side code it would be better to use a JS utility that reads configuration (merging values from multiple sources) instead of using these raw values in application code
 	'@config': 'src/config',
@@ -32,9 +28,13 @@ module.exports = {
 	// Global styles
 	// NOTE: we could split this further
 	'@global-styles': 'src/styles/global.scss',
-	'@shared-styles': 'src/styles/shared.scss'
+	'@shared-styles': 'src/styles/shared.scss',
 
 	// Test cases
 	// '@test': 'test'
+
+	// src root
+	// NOTE: prefer the other aliases over this (so we keep this last)
+	'@': 'src'
 
 }
