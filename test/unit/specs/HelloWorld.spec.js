c6a0b6fb4101700334da8a85bdaaf6b0054f7f51
Jerry Jäppinen <eiskis@gmail.com>
2017-12-15 12:13:30 +0100

    Fixed broken unit test

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,11 @@
-import Vue from '@/vueSetup'
+import { Vue } from '@vue-setup'
 import { HelloWorld } from '@components'
 
 describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(HelloWorld)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
+    expect(vm.$el.querySelector('.c-hello-world h1').textContent)
     .toEqual('Welcome to Your Vue.js App')
   })
 })
