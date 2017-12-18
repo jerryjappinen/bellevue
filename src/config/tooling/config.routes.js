45ccfe75e35f11648df4e72845d3996ec7e179a5
Jerry Jäppinen <eiskis@gmail.com>
2017-12-18 12:38:58 +0100

    Moved routes config

--- src/config/tooling/config.routes.js
+++ src/config/tooling/config.routes.js
@@ -1,3 +1,5 @@
+// NOTE: this is NOT included in @config/index.js
+// NOTE: dependency to @components might cause webpack side effects when imported
 import * as components from '@components'
 
 export default [
