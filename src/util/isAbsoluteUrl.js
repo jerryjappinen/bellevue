208a818731de044f39dc02b7776465fb0a6e2d22
Jerry Jäppinen <eiskis@gmail.com>
2017-12-19 17:12:29 +0100

    Added isAbsoludeUrl checker

---
+++ src/util/isAbsoluteUrl.js
@@ -0,0 +1,4 @@
+// https://stackoverflow.com/questions/10687099/how-to-test-if-a-url-string-is-absolute-or-relative
+export default function (string) {
+	return new RegExp('^(?:[a-z]+:)?//', 'i').test(string) ? true : false
+}
