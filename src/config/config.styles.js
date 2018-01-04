84b4a3bc97a378128ee8d12eb6e2c497815b71bc
Jerry Jäppinen <eiskis@gmail.com>
2018-01-04 18:01:51 +0100

    Removed unneeded mapping code

--- src/config/config.styles.js
+++ src/config/config.styles.js
@@ -5,16 +5,4 @@ import { includes, mapValues } from 'lodash'
 import constants from '!!/Users/jerry.jappinen/Development/sass-values-loader?preserveKeys=false!@styles-constants'
 // import constants from '!!sass-values-loader!@styles-constants'
 
-// // Return normalized values
-// export default mapValues(constants, (value, key) => {
-
-// 	// Convert some values to numbers
-// 	const potentialUnit = value.substr(-2)
-// 	if (includes(['ms', 'px'], potentialUnit)) {
-// 		return parseInt(value)
-// 	}
-
-// 	return value
-// })
-
 export default constants
