8c0ceea832c72592e726f74047caa039d77f2674
Jerry Jäppinen <eiskis@gmail.com>
2018-01-06 15:01:38 +0100

    Sass values loading

--- src/config/config.styles.js
+++ src/config/config.styles.js
@@ -1,6 +1,4 @@
 // Get SCSS constants
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import constants from '!!/Users/jerry.jappinen/Development/sass-values-loader?preserveKeys=false!@styles-constants'
-// import constants from '!!sass-values-loader!@styles-constants'
-
+import constants from '!!sass-to-js!@styles-constants'
 export default constants
