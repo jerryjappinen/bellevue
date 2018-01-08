afcd506064c79d4d710c2c900a168d8a516cb79d
Jerry Jäppinen <eiskis@gmail.com>
2018-01-08 14:22:09 +0100

    Added offline plugin settings

---
+++ src/config/tooling/config.offline.js
@@ -0,0 +1,19 @@
+// Use `offline-plugin` to let users cache your app
+// NOTE: to enable plugin, set options in `main.js`, `config.build.js` and `config.dev.build.js`
+
+// This file only defines the options: https://github.com/NekR/offline-plugin/blob/master/docs/options.md
+module.exports = {
+
+	// Plugin options
+	// appShell: null,
+	// caches: 'all',
+	// publicPath: '',
+
+	// responseStrategy: 'network-first',
+	// updateStrategy: 'all',
+	// externals: [],
+	// excludes: [],
+	// relativePaths: false,
+	autoUpdate: true
+
+}
