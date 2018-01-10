9a89578238af4ba6f04f4ed99b22db0775349fdc
Jerry Jäppinen <eiskis@gmail.com>
2018-01-10 13:45:54 +0100

    Added vuex-specific linting rules

---
+++ src/store/.eslintrc.js
@@ -0,0 +1,9 @@
+// When writing vuex code, some patterns that are normally disallowed can be common
+module.exports = {
+	rules: {
+
+		// We turn this off so we can comfortably mutate state that is passed as an argument
+		'no-param-reassign': ['off']
+
+	}
+}
