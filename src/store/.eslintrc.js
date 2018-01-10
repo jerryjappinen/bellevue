bab8b7795282fd016339aaed66d0fa303bd7ba2f
Jerry Jäppinen <eiskis@gmail.com>
2018-01-10 14:45:55 +0100

    Vuex linting rules

--- src/store/.eslintrc.js
+++ src/store/.eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
 	rules: {
 
 		// We turn this off so we can comfortably mutate state that is passed as an argument
-		'no-param-reassign': ['off']
+		'no-param-reassign': ['off'],
+
+		// We frequently break this rule by reusing common variable names in the same file
+		'no-shadow': ['off']
 
 	}
 }
